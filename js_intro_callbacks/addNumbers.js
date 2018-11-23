36e2d7778ba7ec7ec8ef779a4ac23eda0af71587
kat-onyx <sarah.kathryn.peters@gmail.com>
2018-11-23 15:28:13 -0800

    Write recursive #addNumbers in javascript

---
+++ js_intro_callbacks/addNumbers.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft === 0) {
+    console.log("Hit the base case if block!");
+    completionCallback(sum);
+  } else if (numsLeft > 0) {
+    console.log("NumsLeft: ", numsLeft);
+    reader.question("Enter a number: ", function(userInput) {
+      const num1 = parseInt(userInput);
+      sum += num1;
+      console.log("Partial sum: ", sum);
+      addNumbers(sum, numsLeft-1, completionCallback);
+    });
+  }
+}
+
+addNumbers(0, 3, function(sum) {
+   console.log(`Total Sum: ${sum}`);
+   reader.close();
+});
